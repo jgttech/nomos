80a729bbc5e40bd803581fecd62e1bedc19c111f
Jonathon Tech <65690496+jgttech@users.noreply.github.com>
2023-05-10 18:51:48 -0500

    Update install process

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # dir path for invoking the python install script.
 BASE_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
-# Install Nix package manager
-sh <(curl -L https://nixos.org/nix/install) --daemon
+# # Install Nix package manager
+# sh <(curl -L https://nixos.org/nix/install) --daemon
 
-# Refresh the shell instance.
-source ~/.bashrc
+# # Refresh the shell instance.
+# source ~/.bashrc
 
 # Setup the system using this Python 3 script.
 python3 ${BASE_DIR}/.cli install ${BASE_DIR}
